7ac394d2e141b96d2b0fe1d468db0f4d5776098e
KrystianSzalaj <231944@student.uek.krakow.pl>
2024-10-27 20:07:57 +0100

    Stwory

---
+++ Simulator/Animals.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simulator;
+
+public class Animals
+{
+    // Właściwość required
+    public required string Description { get; init; }
+
+    // Właściwość z domyślną wartością
+    public uint Size { get; set; } = 3;
+
+    // Właściwość do odczytu Info
+    public string Info => $"{Description} <{Size}>";
+}
---
+++ Simulator/Creature.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simulator;
+
+public class Creature
+{
+    // Właściwości automatyczne
+    public string Name { get; set; }
+    public int Level { get; set; } = 1; // Wartość domyślna 1
+
+    // Konstruktor przyjmujący wartości początkowe
+    public Creature(string name, int level = 1)
+    {
+        Name = name;
+        Level = level;
+    }
+
+    // Bezparametrowy konstruktor
+    public Creature()
+    {
+        // Nie robi nic
+    }
+
+    // Metoda SayHi
+    public void SayHi()
+    {
+        Console.WriteLine($"Hello, my name is {Name} and I am level {Level}.");
+    }
+
+    // Właściwość do odczytu Info
+    public string Info => $"{Name}, Level: {Level}";
+}
