5d4fd0c2ef98151f8e46b7f79f3fc71ff7e46468
KrystianSzalaj <231944@student.uek.krakow.pl>
2024-10-27 20:12:50 +0100

    Settery

--- Simulator/Animals.cs
+++ Simulator/Animals.cs
@@ -8,8 +8,38 @@ namespace Simulator;
 
 public class Animals
 {
+    // Prywatne pole dla właściwości
+    private string _description = "Unknown";
+
     // Właściwość required
-    public required string Description { get; init; }
+    public required string Description
+    {
+        get => _description;
+        init
+        {
+            // Usuwanie spacji na początku i końcu
+            string trimmedValue = value.Trim();
+
+            // Upewnij się, że ma co najmniej 3 znaki
+            if (trimmedValue.Length < 3)
+            {
+                trimmedValue = trimmedValue.PadRight(3, '#');
+            }
+            // Upewnij się, że ma najwyżej 15 znaków
+            else if (trimmedValue.Length > 15)
+            {
+                trimmedValue = trimmedValue.Substring(0, 15);
+            }
+
+            // Upewnij się, że ma co najmniej 3 znaki po przycięciu
+            if (trimmedValue.Length < 3)
+            {
+                trimmedValue = trimmedValue.PadRight(3, '#');
+            }
+
+            _description = trimmedValue;
+        }
+    }
 
     // Właściwość z domyślną wartością
     public uint Size { get; set; } = 3;
--- Simulator/Creature.cs
+++ Simulator/Creature.cs
@@ -8,9 +8,66 @@ namespace Simulator;
 
 public class Creature
 {
-    // Właściwości automatyczne
-    public string Name { get; set; }
-    public int Level { get; set; } = 1; // Wartość domyślna 1
+    // Prywatne pola dla właściwości
+    private string _name = "Unknown";
+    private int _level = 1;
+
+    // Właściwość automatyczna Name z walidacją
+    public string Name
+    {
+        get => _name;
+        set
+        {
+            // Upewnij się, że Name można ustawić tylko raz
+            if (_name != "Unknown") return;
+
+            // Usuwanie spacji na początku i końcu
+            string trimmedValue = value.Trim();
+
+            // Upewnij się, że ma co najmniej 3 znaki
+            if (trimmedValue.Length < 3)
+            {
+                trimmedValue = trimmedValue.PadRight(3, '#');
+            }
+            // Upewnij się, że ma najwyżej 25 znaków
+            else if (trimmedValue.Length > 25)
+            {
+                trimmedValue = trimmedValue.Substring(0, 25);
+            }
+
+            // Upewnij się, że ma co najmniej 3 znaki po przycięciu
+            if (trimmedValue.Length < 3)
+            {
+                trimmedValue = trimmedValue.PadRight(3, '#');
+            }
+
+            // Upewnij się, że pierwszy znak jest wielką literą
+            if (char.IsLower(trimmedValue[0]))
+            {
+                trimmedValue = char.ToUpper(trimmedValue[0]) + trimmedValue[1..];
+            }
+
+            _name = trimmedValue;
+        }
+    }
+
+    // Właściwość automatyczna Level z walidacją
+    public int Level
+    {
+        get => _level;
+        set
+        {
+            // Upewnij się, że Level można ustawić tylko raz
+            if (_level != 1) return;
+
+            if (value < 1)
+                _level = 1;
+            else if (value > 10)
+                _level = 10;
+            else
+                _level = value;
+        }
+    }
 
     // Konstruktor przyjmujący wartości początkowe
     public Creature(string name, int level = 1)
@@ -33,4 +90,11 @@ public class Creature
 
     // Właściwość do odczytu Info
     public string Info => $"{Name}, Level: {Level}";
+
+    // Metoda Upgrade
+    public void Upgrade()
+    {
+        if (_level < 10)
+            _level++;
+    }
 }
