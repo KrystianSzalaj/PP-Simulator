2b5595a5a3a5f189a91d81b224607cb40656668f
KrystianSzalaj <146976698+KrystianSzalaj@users.noreply.github.com>
2024-11-19 22:10:58 +0100

    Testy

---
+++ Simulator/TestSimulator/RectangleTest.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualBasic;
+
+class TestRectangle(unittest.TestCase) :
+    def test_creation(self):
+        rect = Rectangle(Point(0, 0), Point(3, 3))
+        self.assertEqual(rect.bottom_left, Point(0, 0))
+        self.assertEqual(rect.top_right, Point(3, 3))
+
+    def test_area(self):
+        rect = Rectangle(Point(0, 0), Point(3, 3))
+        self.assertEqual(rect.area(), 9)
+
+    def test_intersection(self):
+        rect1 = Rectangle(Point(0, 0), Point(3, 3))
+        rect2 = Rectangle(Point(2, 2), Point(5, 5))
+        intersection = rect1.intersection(rect2)
+        self.assertEqual(intersection, Rectangle(Point(2, 2), Point(3, 3)))
+
+    def test_no_intersection(self):
+        rect1 = Rectangle(Point(0, 0), Point(3, 3))
+        rect2 = Rectangle(Point(4, 4), Point(5, 5))
+        self.assertIsNone(rect1.intersection(rect2))
+
+    def test_invalid_creation(self):
+        with self.assertRaises(ValueError):
+            Rectangle(Point(3, 3), Point(0, 0))
---
+++ Simulator/TestSimulator/pointTest.cs
@@ -0,0 +1,22 @@
+﻿using System.Drawing;
+
+import unittest
+
+class TestPoint(unittest.TestCase) :
+    def test_creation(self):
+        point = Point(2, 3)
+        self.assertEqual(point.x, 2)
+        self.assertEqual(point.y, 3)
+
+    def test_equality(self):
+        p1 = Point(2, 3)
+        p2 = Point(2, 3)
+        p3 = Point(3, 2)
+        self.assertEqual(p1, p2)
+        self.assertNotEqual(p1, p3)
+
+    def test_invalid_creation(self):
+        with self.assertRaises(ValueError):
+            Point("a", 3)
+        with self.assertRaises(ValueError):
+            Point(2, None)
